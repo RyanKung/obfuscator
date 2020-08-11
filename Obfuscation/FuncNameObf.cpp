a705ede1ed72e2da341ee79a0ae8b57835c7c8ce
Ryan.K <ryankung@ieee.org>
2020-08-12 01:20:18 +0800

    clean code

--- Obfuscation/FuncNameObf.cpp
+++ Obfuscation/FuncNameObf.cpp
@@ -77,39 +77,6 @@ namespace llvm {
 	    DEBUG_OUT("Renaming Struct: "<<GV.getInitializer()->getName()<<" to: "<<_name);
 	  }
         }
-	else if (GV.getName().str().find("OBJC_SELECTOR_REFERENCES") == 0) {
-	  if (GV.hasInitializer()) {
-	    ConstantExpr *CE = dyn_cast<ConstantExpr>(GV.getInitializer());
-	    Constant *C = CE->getOperand(0);
-	    GlobalVariable *SELNameGV = dyn_cast<GlobalVariable>(C);
-	    ConstantDataArray *CDA =
-              dyn_cast<ConstantDataArray>(SELNameGV->getInitializer());
-	    StringRef SELName = CDA->getAsString(); // This is REAL Selector Name
-	    for (auto U = GV.user_begin(); U != GV.user_end(); U++) {
-	      if (Instruction *I = dyn_cast<Instruction>(*U)) {
-	    	IRBuilder<> builder(I);
-		FunctionType *fn_type = FunctionType::get(I->getType(),
-								     {Type::getInt8PtrTy(M.getContext())},
-								     false);
-		llvm::FunctionCallee sel_registerName_Func = M.getOrInsertFunction("sel_registerName",
-										fn_type);
-
-		//Function *sel_registerName_Func = cast<Function>(M.getFunction("sel_registerName"));
-	    	Value *newGlobalSELName = builder.CreateGlobalStringPtr(SELName);
-		CallInst *CI = builder.CreateCall(sel_registerName_Func, {newGlobalSELName});
-	    	Value *BCI = builder.CreateBitCast(CI, I->getType());
-	    	I->replaceAllUsesWith(BCI);
-	    	I->eraseFromParent();
-		DEBUG_OUT("Renaming SEL: " << SELName.str() << " to: " << newGlobalSELName);
-	      }
-	    }
-	    GV.removeDeadConstantUsers();
-	    if (GV.getNumUses() == 0) {
-	      GV.dropAllReferences();
-	      GV.eraseFromParent();
-	    }
-	  }
-	}
       }
       return true;
     }
